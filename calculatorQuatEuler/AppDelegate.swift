5f0799c7e4b32121dfd432e3bb602c2200a4d606
alibenD <aliben.develop@gmail.com>
2019-05-05 11:09:48 +0800

    Initial Commit

---
+++ calculatorQuatEuler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  calculatorQuatEuler
+//
+//  Created by Aliben on 5/5/19.
+//  Copyright © 2019 Aliben. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ calculatorQuatEuler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calculatorQuatEuler
+//
+//  Created by Aliben on 5/5/19.
+//  Copyright © 2019 Aliben. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
